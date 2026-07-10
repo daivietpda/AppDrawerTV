a92dc64e2fe84770e0c19843ef49178bf6c9a7f4
daivietpda <3686023+daivietpda@users.noreply.github.com>
2026-07-10 10:41:38 +0700

    Upgrade Android build and app launch discovery
    Modernize the project to current Android tooling by upgrading AGP, Gradle, Kotlin, SDK targets, Java/Kotlin JVM levels, and related manifest requirements (`android:exported`, package visibility permission). App listing now resolves launchable activities from both standard and Leanback launcher categories, deduplicates packages, and adds Leanback launch fallback when opening apps.

--- app/src/main/java/org/lineageos/appdrawer/AppDrawerActivity.java
+++ app/src/main/java/org/lineageos/appdrawer/AppDrawerActivity.java
@@ -22,6 +22,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -31,7 +32,9 @@ import android.widget.Toast;
 import android.widget.ListAdapter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class AppDrawerActivity extends Activity {
   private PackageManager packageManager = null;
@@ -44,13 +47,17 @@ public class AppDrawerActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView((int) R.layout.activity_main);
     this.packageManager = getPackageManager();
-    new LoadApplications(this, null).execute();
+    new LoadApplications().execute();
     this.grid = (GridView) findViewById(R.id.grid);
     this.grid.setAdapter((ListAdapter) this.listadaptor);
     this.grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
         try {
-          Intent intent = AppDrawerActivity.this.packageManager.getLaunchIntentForPackage(((ApplicationInfo) AppDrawerActivity.this.applist.get(position)).packageName);
+          String packageName = AppDrawerActivity.this.applist.get(position).packageName;
+          Intent intent = AppDrawerActivity.this.packageManager.getLaunchIntentForPackage(packageName);
+          if (intent == null) {
+            intent = AppDrawerActivity.this.packageManager.getLeanbackLaunchIntentForPackage(packageName);
+          }
           if (intent != null) {
             AppDrawerActivity.this.startActivity(intent);
           }
@@ -63,32 +70,29 @@ public class AppDrawerActivity extends Activity {
     });
   }
 
-  private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
-    ArrayList<ApplicationInfo> applist = new ArrayList<>();
-    for (ApplicationInfo info : list) {
-      try {
-        if (null != packageManager.getLaunchIntentForPackage(info.packageName)) {
-          applist.add(info);
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    Collections.sort(applist, new ApplicationInfo.DisplayNameComparator(packageManager));
-    return applist;
-  }
-
   private class LoadApplications extends AsyncTask<Void, Void, Void> {
     private ProgressDialog progress = null;
 
-    public LoadApplications(AppDrawerActivity appDrawerActivity, Object o) {
-    }
-
     @Override
     protected Void doInBackground(Void... params) {
-      applist =
-          checkForLaunchIntent(
-              packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+      applist = new ArrayList<>();
+
+      Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+      mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+      List<ResolveInfo> launchables = packageManager.queryIntentActivities(mainIntent, 0);
+
+      Intent leanbackIntent = new Intent(Intent.ACTION_MAIN, null);
+      leanbackIntent.addCategory(Intent.CATEGORY_LEANBACK_LAUNCHER);
+      launchables.addAll(packageManager.queryIntentActivities(leanbackIntent, 0));
+
+      Set<String> packageNames = new HashSet<>();
+      for (ResolveInfo resolveInfo : launchables) {
+        if (packageNames.add(resolveInfo.activityInfo.packageName)) {
+          applist.add(resolveInfo.activityInfo.applicationInfo);
+        }
+      }
+
+      Collections.sort(applist, new ApplicationInfo.DisplayNameComparator(packageManager));
       listadaptor = new ApplicationAdapter(AppDrawerActivity.this, R.layout.grid_item, applist);
 
       return null;
